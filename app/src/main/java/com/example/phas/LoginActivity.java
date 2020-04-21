80cef54693dbbea047a721a8d0ff2aa208a6336a
yong77aa <yong77aa@naver.com>
2020-04-21 20:11:10 +0900

    기본 UI 구현 (2020.04.20) 인트로, 로그인, 회원가입

---
+++ app/src/main/java/com/example/phas/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.example.phas;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button register = findViewById(R.id.register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
