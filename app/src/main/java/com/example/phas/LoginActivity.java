cb51a3b0e2b8140e50d0c6853fe7cf5f51e44ad7
yong77aa <yong77aa@naver.com>
2020-04-22 00:54:25 +0900

    기본 UI 구현 (2020.04.22) 툴바, drawer layout 구현

--- app/src/main/java/com/example/phas/LoginActivity.java
+++ app/src/main/java/com/example/phas/LoginActivity.java
@@ -9,16 +9,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button register;
+    Button log_in;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button register = findViewById(R.id.register);
+        register = findViewById(R.id.register); // 계정 생성 버튼
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        log_in = findViewById(R.id.log_in); // 로그인 버튼
+        log_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/phas/MainActivity.java
+++ app/src/main/java/com/example/phas/MainActivity.java
@@ -1,14 +1,67 @@
 package com.example.phas;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity{
+
+    Toolbar toolBar;
+    ActionBar actionBar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolBar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolBar);
+
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼 만들기
+        actionBar.setHomeAsUpIndicator(R.drawable.menu_icon); //뒤로가기 버튼 이미지 지정
+
+        drawerLayout = findViewById(R.id.drawer_layout_main);
+
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                menuItem.setChecked(true);
+                drawerLayout.closeDrawers();
+
+                int id = menuItem.getItemId();
+                String title = menuItem.getTitle().toString();
+
+                if (id == R.id.nav_first) {
+                    Toast.makeText(MainActivity.this, "첫번째", Toast.LENGTH_LONG).show();
+                } else if (id == R.id.nav_second) {
+                    Toast.makeText(MainActivity.this, "두번째", Toast.LENGTH_LONG).show();
+                }
+                return true;
+            }
+        });
     }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            switch (item.getItemId()){
+                case android.R.id.home:{ // 왼쪽 상단 버튼 눌렀을 때
+                    drawerLayout.openDrawer(GravityCompat.START);
+                    return true;
+                }
+            }
+            return super.onOptionsItemSelected(item);
+        }
 }
